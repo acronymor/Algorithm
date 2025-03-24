e8ded360cd7141f4eef94c760ddff24adbfcd5e0
acronymor <acronymor@gmail.com>
2025-03-25 00:31:56 +0800

    [ADD][LeetCode-1143] longest common subsequence

---
+++ src/array/lc_1143_longest_common_subsequence.go
@@ -0,0 +1,43 @@
+package array
+
+/**
+ * @brief 最长公共子序列
+ *
+ * @details
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * 一个字符串的 子序列 是指这样一个新的字符串：
+ * 它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ * @par 示例
+ *
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ *
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ *
+ * @see [leetcode-1143](https://leetcode.cn/problems/longest-common-subsequence/)
+ */
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	var dp [][]int = make([][]int, len(text1)+1)
+	for i := 0; i <= len(text1); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(text1)][len(text2)]
+}
