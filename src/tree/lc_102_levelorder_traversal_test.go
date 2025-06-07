c088cb0aef1843b5f5dcf935e172d54efd092c24
acronymor <acronymor@gmail.com>
2025-06-08 01:27:08 +0800

    [UPDATE][LeetCode-102] level order traversal

--- src/tree/lc_102_levelorder_traversal_test.go
+++ src/tree/lc_102_levelorder_traversal_test.go
@@ -5,7 +5,36 @@ import (
 	"testing"
 )
 
-func TestLevelOrder(t *testing.T) {
+func BuildBinaryTree(nums []int) *TreeNode {
+	var idx int = 0
+	var root = &TreeNode{Val: nums[idx], Left: nil, Right: nil}
+	var queue = []*TreeNode{root}
+
+	for len(queue) != 0 {
+		var size = len(queue)
+
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			idx++
+			if idx < len(nums) && nums[idx] != -1 {
+				node.Left = &TreeNode{Val: nums[idx], Left: nil, Right: nil}
+				queue = append(queue, node.Left)
+			}
+
+			idx++
+			if idx < len(nums) && nums[idx] != -1 {
+				node.Right = &TreeNode{Val: nums[idx], Left: nil, Right: nil}
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return root
+}
+
+func TestLevelOrder1(t *testing.T) {
 	var nums []int = []int{1, -1, 2, 3, -1, -1, -1}
 	var root *TreeNode = ToBinaryTree(nums)
 	var res [][]int = levelOrder(root)
@@ -20,6 +49,27 @@ func TestLevelOrder(t *testing.T) {
 
 	nums = []int{3, 9, -1, -1, 20, 15, -1, -1, 7, -1, -1}
 	root = ToBinaryTree(nums)
+	res = levelOrder(root)
+	assert.ElementsMatch(t, []int{3}, res[0])
+	assert.ElementsMatch(t, []int{9, 20}, res[1])
+	assert.ElementsMatch(t, []int{15, 7}, res[2])
+}
+
+func TestLevelOrder2(t *testing.T) {
+	var nums []int = []int{1, -1, 2, 3, -1}
+	var root *TreeNode = BuildBinaryTree(nums)
+	var res [][]int = levelOrder(root)
+	assert.ElementsMatch(t, []int{1}, res[0])
+	assert.ElementsMatch(t, []int{2}, res[1])
+	assert.ElementsMatch(t, []int{3}, res[2])
+
+	nums = []int{1}
+	root = BuildBinaryTree(nums)
+	res = levelOrder(root)
+	assert.ElementsMatch(t, []int{1}, res[0])
+
+	nums = []int{3, 9, 20, -1, -1, 15, 7}
+	root = BuildBinaryTree(nums)
 	res = levelOrder(root)
 	assert.ElementsMatch(t, []int{3}, res[0])
 	assert.ElementsMatch(t, []int{9, 20}, res[1])
