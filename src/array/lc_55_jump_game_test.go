5e90d40b9eb845465e7b39656439003a119a62d2
acronymor <acronymor@gmail.com>
2025-07-15 22:05:07 +0800

    [UPDATE][LeetCode-55] jump game

--- src/array/lc_55_jump_game_test.go
+++ src/array/lc_55_jump_game_test.go
@@ -5,20 +5,38 @@ import (
 	"testing"
 )
 
-func TestCanJump(t *testing.T) {
+func TestCanJump1(t *testing.T) {
 	var nums []int = []int{2, 3, 1, 1, 4}
-	var res = canJump(nums)
+	var res = canJump1(nums)
 	assert.True(t, res)
 
 	nums = []int{3, 2, 1, 0, 4}
-	res = canJump(nums)
+	res = canJump1(nums)
 	assert.False(t, res)
 
 	nums = []int{1, 2}
-	res = canJump(nums)
+	res = canJump1(nums)
 	assert.True(t, res)
 
 	nums = []int{0, 2, 3}
-	res = canJump(nums)
+	res = canJump1(nums)
+	assert.False(t, res)
+}
+
+func TestCanJump2(t *testing.T) {
+	var nums []int = []int{2, 3, 1, 1, 4}
+	var res = canJump2(nums)
+	assert.True(t, res)
+
+	nums = []int{3, 2, 1, 0, 4}
+	res = canJump2(nums)
+	assert.False(t, res)
+
+	nums = []int{1, 2}
+	res = canJump2(nums)
+	assert.True(t, res)
+
+	nums = []int{0, 2, 3}
+	res = canJump2(nums)
 	assert.False(t, res)
 }
